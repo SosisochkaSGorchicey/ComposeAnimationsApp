5bc67a0a2c84b4487413e1fe0f376264bdbda20f
sosisochka <kudrina.alice@yandex.ru>
2024-04-03 14:53:39 +0300

    Added background animation and grid item placement

--- app/src/main/java/com/example/composeanimationsapp/screens/catGallery/CatGalleryUI.kt
+++ app/src/main/java/com/example/composeanimationsapp/screens/catGallery/CatGalleryUI.kt
@@ -1,12 +1,25 @@
 package com.example.composeanimationsapp.screens.catGallery
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
 import com.example.composeanimationsapp.screens.catGallery.mvi.CatGalleryState
@@ -19,19 +32,36 @@ fun CatGalleryUI(
 ) {
     val catPagingData = state.catData.collectAsLazyPagingItems()
 
-    LazyColumn(
+    val infiniteTransition = rememberInfiniteTransition(label = "infiniteColorAnimation")
+    val color by infiniteTransition.animateColor(
+        initialValue = MaterialTheme.colorScheme.background,
+        targetValue = MaterialTheme.colorScheme.onSurface,
+        animationSpec = infiniteRepeatable(
+            animation = tween(3000, easing = LinearOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "colorAnimation"
+    )
+
+    LazyVerticalStaggeredGrid(
         modifier = Modifier
             .padding(padding)
             .fillMaxSize()
+            .background(color),
+        columns = StaggeredGridCells.Fixed(2),
+        verticalItemSpacing = 8.dp,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(
             count = catPagingData.itemCount,
             key = { it }
         ) {
             AsyncImage(
-                modifier = Modifier.animateItemPlacement(),
+                modifier = Modifier
+                    .animateItemPlacement(),
                 model = catPagingData[it]?.imageUrl,
-                contentDescription = null
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth
             )
         }
     }
